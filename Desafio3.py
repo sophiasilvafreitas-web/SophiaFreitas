6dd78961d75fb8a640b4d0524543a1c2cd2f7802
sophiasilvafreitas-web <sophia.silva.freitas@escola.pr.gov.br>
2026-04-06 20:35:46 -0300

    Lista de 5 nomes e imprimir
    Add script to collect and display user names

---
+++ Desafio3.py
@@ -0,0 +1,20 @@
+# Cria uma lista vazia para armazenar os nomes
+nomes = []
+
+# Solicita ao usuário que digite o primeiro nome e adiciona à lista
+nomes.append(input("Digite o 1º nome: "))
+
+# Solicita o segundo nome e adiciona à lista
+nomes.append(input("Digite o 2º nome: "))
+
+# Solicita o terceiro nome e adiciona à lista
+nomes.append(input("Digite o 3º nome: "))
+
+# Solicita o quarto nome e adiciona à lista
+nomes.append(input("Digite o 4º nome: "))
+
+# Solicita o quinto nome e adiciona à lista
+nomes.append(input("Digite o 5º nome: "))
+
+# Exibe a lista completa de nomes digitados
+print("Lista de nomes:", nomes)
