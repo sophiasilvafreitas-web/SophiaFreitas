0d5f4414d6830d52ba821523c620bb159b1c5260
sophiasilvafreitas-web <sophia.silva.freitas@escola.pr.gov.br>
2026-04-06 20:42:14 -0300

    Área de um triângulo

---
+++ Desafio5.py
@@ -0,0 +1,11 @@
+# Solicita ao usuário que digite o valor da base do triângulo
+base = float(input("Digite a base do triângulo: "))
+
+# Solicita ao usuário que digite o valor da altura do triângulo
+altura = float(input("Digite a altura do triângulo: "))
+
+# Calcula a área do triângulo usando a fórmula: (base * altura) / 2
+area = (base * altura) / 2
+
+# Exibe o resultado da área calculada
+print("A área do triângulo é:", area)
